f176c444456a51c702c2158bc623fe0c6e6b6436
Tân <trinhhoangtan96@gmail.com>
2018-04-02 16:03:08 +0700

    Send messages to only people in same room

--- server/server.js
+++ server/server.js
@@ -58,8 +58,13 @@ io.on('connection', (socket) => {
   });
 
   socket.on('createMessage', (newMessage, callback) => {
-    console.log('createMessage', newMessage);
-    io.emit('newMessage', generateMessage(newMessage.from, newMessage.text));
+    //console.log('createMessage', newMessage);
+    var user = users.getUser(socket.id);
+
+    if (user && isRealString(newMessage.text)) {
+      io.to(user.room).emit('newMessage', generateMessage(user.name, newMessage.text));
+    }
+
     callback();
     // socket.broadcast.emit('newMessage', {
     //   from: newMessage.from,
@@ -69,8 +74,12 @@ io.on('connection', (socket) => {
   });
 
   socket.on('createLocationMessage', (coords) => {
-    io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
-  })
+    var user = users.getUser(socket.id);
+
+    if (user) {
+      io.to(user.room).emit('newLocationMessage', generateLocationMessage(user.name, coords.latitude, coords.longitude));
+    }
+  });
 
   socket.on('disconnect', () => {
     //console.log('User was disconnected');
